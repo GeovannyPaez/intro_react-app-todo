ccc4d8da87cda048af7086463e1c52569d3b608d
Geovanny Paez <yuniorpaezpabon@gmail.com>
2022-10-05 16:15:37 -0500

    implementamos useReducer en el archivo useLocalStorage

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -2,10 +2,22 @@ import React from "react";
 
 export function useLocalStorage(itemName, initialValue){
     
-    const [loading, setLoading]= React.useState(true)
-    const [error, setError]= React.useState(false)
-    const [item,setItem]= React.useState(initialValue);
-    
+
+    const [state,dispatch]= React.useReducer( reducer , initialState({initialValue}) );
+
+    const {
+        loading,
+        error,
+        item
+    }=state;
+
+    // const [loading, setLoading]= React.useState(true)
+    // const [error, setError]= React.useState(false)
+    // const [item,setItem]= React.useState(initialValue);
+    const setError=error=> dispatch({type:actionTypes.setError,payload: error});
+    const setLoading=()=>dispatch({type: actionTypes.setLoading});
+    const setItem=newItem=> dispatch({type:actionTypes.setItem, payload: newItem});
+
     const getItem=()=>{
         return JSON.parse(localStorage.getItem(itemName)) ;
     }
@@ -14,9 +26,9 @@ export function useLocalStorage(itemName, initialValue){
         setTimeout(()=>{
             try {
                 let parsetItem= getItem() || initialValue; 
-    
                 setItem(parsetItem);
-                setLoading(false)
+                console.log(parsetItem);
+                setLoading(false);
             } catch (error) {
                 setError(error)
             }
@@ -41,4 +53,33 @@ export function useLocalStorage(itemName, initialValue){
         }
         
     ]
+}
+const initialState= ({initialValue})=>({
+    loading: true,
+    error:false,
+    item: initialValue
+});
+const actionTypes = {
+    setItem: 'ITEM',
+    setLoading: 'LOADING',
+    setError: 'ERROR'
+}
+const reudcerObject=(state, payload)=>{
+    return {
+        [actionTypes.setItem]:{
+            ...state,
+            item: payload
+        },
+        [actionTypes.setError]:{
+            ...state,
+            error: true
+        },
+        [actionTypes.setLoading]:{
+            ...state,
+            loading: false
+        }
+    }
+}
+const reducer=(state,action)=>{
+    return reudcerObject(state,action.payload)[action.type] || state;
 }
